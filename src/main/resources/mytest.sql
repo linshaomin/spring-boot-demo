d4da4d6ce028e58e2e52d2357bb995d08e1d7b85
shaomin.lin <shaomin.lin@qishon.com>
2018-11-02 10:51:12 +0800

    建表语句

---
+++ src/main/resources/mytest.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `t_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1009 DEFAULT CHARSET=utf8;
