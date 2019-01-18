acea6473f478b0f8575a5b6d826ae24135b7ca18
shaomin.lin <shaomin.lin@qishon.com>
2019-01-18 11:16:59 +0800

    集成redis

--- src/test/java/com/example/demo/redis/TestRedis.java
+++ src/test/java/com/example/demo/redis/TestRedis.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -16,13 +17,16 @@ public class TestRedis {
     @Autowired
     private RedisTemplate redisTemplate;
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @Test
     public void set(){
-        redisTemplate.opsForValue().set("test","testValue1");
+        stringRedisTemplate.opsForValue().set("test2","testValue1");
     }
 
     @Test
     public void get(){
-        System.out.println(redisTemplate.opsForValue().get("mystring"));
+        System.out.println(stringRedisTemplate.opsForValue().get("mystring"));
     }
 }
